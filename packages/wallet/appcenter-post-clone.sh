704e830b4d17976243fbf5d49bc7e06db77299a3
Paweł Lula <pavloblack@hotmail.com>
2020-05-30 20:05:08 +0200

    Create .env file from app center

--- packages/wallet/appcenter-post-clone.sh
+++ packages/wallet/appcenter-post-clone.sh
@@ -14,9 +14,15 @@ node scripts/appcenter-postclone.js
 # install pods
 cd ios
 pod install
-cd ../
+cd ..
 
 # move yarn.lock to wallet package to force AppCenter to use yarn
 mv ../../yarn.lock ./yarn.lock
 
-
+# Creates an .env from ENV variables for use with react-native-config
+ENV_WHITELIST="^NF"
+printf "Creating an .env file with the following whitelist:\n"
+printf "%s\n\n" $ENV_WHITELIST
+set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" > .env
+printf "\n.env created with contents:\n"
+cat .env
